dfac054ba990d103e12ce55b3f0876f7123a0622
Yuvrajsinh Desai <yuvrajd.invints@gmail.com>
2025-06-19 15:55:12 +0530

    Test 6

--- src/server.ts
+++ src/server.ts
@@ -156,8 +156,8 @@ async function updateShopifyMetafields(combo) {
 }
 
 // Start server
-app.listen(process.env.PORT??5000, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+  console.log(`Server running on port 3000`);
 });
 
 
